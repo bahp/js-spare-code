8ff8621619fa5316b95564f743fc6de7a8f9fbe9
bahp <bernard.hernandez.perez@gmail.com>
2023-06-28 14:28:54 +0100

    ~ enable adding new patients

--- examples/apps/pregnant-care/data.js
+++ examples/apps/pregnant-care/data.js
@@ -1,85 +1,63 @@
-var data = [
-    {
-        patient: {
-            name: 'Veronica',
-            surname: 'Quieti',
-            id: 'VQ'
-        },
-        dateConception: new Date(1990, 3, 28),
-        dateDelivery: '2',
-        firstBaby: true,
-        steps: [
-            {
-                week: 2,
-                range: {
-                    low: 3,
-                    high: 3
-                },
-                body: `<strong>This is the first item's accordion body.</strong> It is shown by default, until
-                the collapse plugin adds the appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing and hiding via CSS transitions.
-                You can modify any of this with custom CSS or overriding our default variables. It's also
-                worth noting that just about any HTML can go within the <code>.accordion-body</code>, though
-                the transition does limit overflow.`
-            },
-            {
-                week: 16,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 24,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 28,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-
-            {
-                week: 32,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 36,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 38,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 40,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            },
-            {
-                week: 41,
-                range: {
-                    low: 3,
-                    high: 3
-                }
-            }
-        ]
-    }
+var WEEKS_INFO = [
+  {
+    week: 2,
+    info: `
+      <ul>
+        <li>Confirm pregnancy and estimate the due date based on the last menstrual period (LMP).</li>
+        <li>Discuss the importance of prenatal care and provide information about available resources.</li>
+        <li>Conduct a general health assessment of the mother, including medical history and any pre-existing conditions.</li>
+        <li>Discuss the mother's lifestyle habits (such as smoking, alcohol, and drug use) and provide guidance for maintaining a healthy pregnancy.</li>
+        <li>Order initial blood tests, including a pregnancy confirmation test and blood type/Rh factor screening.</li>
+        <li>Discuss the schedule for future prenatal visits and provide contact information for emergencies.</li>
+      </ul>
+    `
+  },
+  {
+    week: 16,
+    info: ` 
+      <ul>
+        <li>Confirmation of pregnancy</li>
+        <li>Assessment of general health and well-being</li>
+        <li>Review of medical history and any existing conditions</li>
+        <li>Discussion of prenatal care and nutrition</li>
+        <li>Measurement of blood pressure</li>
+        <li>Checking weight and BMI</li>
+        <li>Listening to the fetal heartbeat (if possible at this stage)</li>
+        <li>Discussion of any concerns or questions</li>
+      </ul>`
+  },
+  {
+    week: 24
+  },
+  {
+    week: 28
+  },
+  {
+    week: 32
+  },
+  {
+    week: 36,
+    info: ``
+  },
+  {
+    week: 38,
+    info: ``
+  },
+  {
+    week: 40,
+    info: ``
+  },
+  {
+    week: 41,
+    info: `
+      <ul>
+        <li>Monitor the mother's blood pressure, weight, and check for signs of edema.</li>
+        <li>Perform a pelvic examination to assess the cervix's effacement and dilation.</li>
+        <li>Assess fetal well-being through a non-stress test or other appropriate tests.</li>
+        <li>Discuss the risks and benefits of continuing the pregnancy beyond the due date.</li>
+        <li>Provide information about potential interventions, such as membrane sweeping or induction of labor.</li>
+        <li>Address any concerns or anxieties the mother may have about prolonged pregnancy.</li>
+        <li>Offer emotional support and reassurance as the mother waits for labor to begin.</li>
+      </ul>`
+  }
 ]
-
