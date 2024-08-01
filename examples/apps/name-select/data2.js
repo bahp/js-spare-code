5156afedda6e120ec75374650a7e878fea4b6efc
bahp <bernard.hernandez.perez@gmail.com>
2024-08-01 14:47:15 +0100

    ~ name select unfinished

--- examples/apps/name-select/data2.js
+++ examples/apps/name-select/data2.js
@@ -1,12 +1,12 @@
 const babyNames = [
-    { id: 1, name: 'Sofia', description: 'Wisdom' },
-    { id: 2, name: 'Luca', description: 'Light' },
-    { id: 3, name: 'Mateo', description: 'Gift of God' },
-    { id: 4, name: 'Emma', description: 'Universal' },
+    { id: 1, name: 'Sofia', description: 'Wisdom' , tags: ['Female'], votes: {'bahp': true, 'very': false}},
+    { id: 2, name: 'Luca', description: 'Light' , votes: {'very': true}},
+    { id: 3, name: 'Mateo', description: 'Gift of God', votes: {'very': false}},
+    { id: 4, name: 'Emma', description: 'Universal', tags: ['Female', 'Short']},
     { id: 5, name: 'Gabriel', description: 'God is my strength' },
     { id: 6, name: 'Martina', description: 'Warlike' },
-    { id: 7, name: 'Leo', description: 'Lion' },
+    { id: 7, name: 'Leo', description: 'Lion', tags: ['Italian', 'Renaissance'] },
     { id: 8, name: 'Giulia', description: 'Youthful' },
-    { id: 9, name: 'Daniel', description: 'God is my judge' },
+    { id: 9, name: 'Daniel', description: 'God is my judge', tags: ['Male']},
     { id: 10, name: 'Isabella', description: 'Devoted to God' }
 ];
